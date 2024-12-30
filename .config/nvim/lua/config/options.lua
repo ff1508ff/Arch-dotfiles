421d9f0c336fb1e8f7efe42dd05da0283c1463d0
ff1508ff <111969821+ff1508ff@users.noreply.github.com>
2024-12-30 21:43:01 +0100

    disable animations from snacks

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,15 +11,17 @@ vim.opt.relativenumber = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- deactivate sycing of system clipboard
+-- deactivate syncing of system clipboard
 vim.opt.clipboard = "unnamed"
 
--- 4 Space indendation
+-- 4 Space indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4 -- 4 because Rust uses this as default
 vim.opt.expandtab = true -- this is standard in lazyvim. I just added it if I ever want to change it
 
-vim.opt.spelllang = "en_gb,de_ch" -- spell checking for english and german
+vim.opt.spelllang = "en_gb,de_ch" -- spell checking for English and German
 vim.opt.spell = true -- enable spell checking
 
 vim.g.typst_pdf_viewer = "zathura" -- this is for typst.vim (zanthera is a pdf viewer which updates the pdf file automatically)
+
+vim.g.snacks_animate = false -- disable animations from snacks, because I think they are to much
