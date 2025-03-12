8eb5199509ffdcd12ac45d76da5ba8d9dbe4f638
ff1508ff <111969821+ff1508ff@users.noreply.github.com>
2025-03-12 20:39:40 +0100

    make update script more readable and remove some configs

--- update.sh
+++ update.sh
@@ -1,12 +1,16 @@
 # update pacman packages
+echo "========== pacman =========="
 sudo pacman -Syu
 
 # update yay(AUR) packages
+echo "========== yay =========="
 sudo yay -Syu
 
 # update rust
+echo "========== rust =========="
 rustup self update
 rustup update stable
 
 # update cargo executables (e.g. zellij)
+echo "========== rust executables =========="
 cargo install-update -a
